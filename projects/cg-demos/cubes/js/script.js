5b9f4742800e1e75f57ba63fa06da7e0473feb5d
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2015-12-21 14:57:16 -0200

    cubes still missing, selection, deletion and working rotation

--- projects/cg-demos/cubes/js/script.js
+++ projects/cg-demos/cubes/js/script.js
@@ -30,6 +30,7 @@ var cubes = {
     offset: new THREE.Vector3(),
     objects: [],
     raycaster: new THREE.Raycaster(),
+    prevMousePosition: null,
 
     init: function() { // Initialization
 
@@ -70,7 +71,7 @@ var cubes = {
         document.addEventListener('mouseup', this.onDocumentMouseUp, false);
 
         // prepare controls (OrbitControls)
-        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.controls = new THREE.OrbitControls(this.camera);
         this.controls.target = new THREE.Vector3(0, 0, 0);
 
         // prepare clock
@@ -96,19 +97,10 @@ var cubes = {
         this.camera.add(dirLight.target);
         
         // add plane, that helps to determinate an intersection position
-        this.plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(5000, 5000, 8), new THREE.MeshBasicMaterial({color: this.scene.fog.color, opacity: 0, transparent: true}));
-//        this.plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(5000, 5000, 8), new THREE.MeshBasicMaterial({color: 0xff0000}));
+        this.plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(5000, 5000, 8), new THREE.MeshBasicMaterial({ color: this.scene.fog.color, opacity: 0, transparent: true }));
+//        this.plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(500, 500, 8), new THREE.MeshBasicMaterial({color: 0xff0000}));
         this.plane.visible = true;
         this.scene.add(this.plane);
-        
-        var cube1 = this.drawCube(0, 0, 0),
-            cube2 = this.drawCube(150, 200, 50);
-        
-        this.objects.push(cube1);
-        this.objects.push(cube2);
-        
-        this.scene.add(cube1);
-        this.scene.add(cube2);
         
     },
     
@@ -126,8 +118,92 @@ var cubes = {
         return cube;
     },
     
+    toRadians: function(degrees) {
+        return degrees * (Math.PI/180);
+    },
+    
     onDocumentMouseDown: function (event) {
         
+        // get mouse position
+        var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+        var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        // get 3D vector from 3D mouse position using 'unproject' function
+        var vector = new THREE.Vector3(mouseX, mouseY, 0.5);
+        vector.unproject(cubes.camera);
+
+        // direction vector
+        var dir = vector.sub(cubes.camera.position).normalize();
+        
+        // set raycaster position
+        cubes.raycaster.set(cubes.camera.position, dir);
+
+        switch (event.button) {
+                
+            case 0:
+                // find intersected objects
+                var intersects = cubes.raycaster.intersectObjects(cubes.objects);
+
+                if (intersects.length > 0) {
+
+                    // disable controls
+                    cubes.controls.enabled = false;
+                    
+                    // set selection - first intersected object (closest)
+                    cubes.selection = intersects[0].object;
+
+                    // calculate offset
+                    var intersects = cubes.raycaster.intersectObject(cubes.plane);
+                    cubes.offset.copy(intersects[0].point).sub(cubes.plane.position);
+
+                } else {
+                    
+                    var intersects = cubes.raycaster.intersectObject(cubes.plane);
+                    var pos = intersects[0].point;
+
+                    var cube = cubes.drawCube(pos.x, pos.y, pos.z);
+                    cubes.objects.push(cube);
+                    cubes.scene.add(cube);
+                    
+                }
+                break;
+                
+            case 1:
+                break;
+                
+            case 2:
+                // find intersected objects
+                var intersects = cubes.raycaster.intersectObjects(cubes.objects);
+
+                if (intersects.length > 0) {
+
+                    // disable controls
+                    cubes.controls.enabled = false;
+                    
+                    // set selection - first intersected object (closest)
+                    cubes.selection = intersects[0].object;
+
+                    // calculate offset
+                    var intersects = cubes.raycaster.intersectObject(cubes.plane);
+                    cubes.offset.copy(intersects[0].point).sub(cubes.plane.position);
+                    
+                    var pos = intersects[0].point;
+                    cubes.prevMousePosition = {
+                        x: pos.x,
+                        y: pos.y,
+                        z: pos.z
+                    };
+                }
+                break;
+                
+        }
+        
+    },
+    
+    onDocumentMouseMove: function (event) {
+        
+        event.preventDefault();
+
         // get mouse position
         var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
         var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -137,65 +213,56 @@ var cubes = {
         vector.unproject(cubes.camera);
 
         // set raycaster position
-        cubes.raycaster.set(cubes.camera.position, vector.sub(cubes.camera.position).normalize());
-
-        // find intersected objects
-        var intersects = cubes.raycaster.intersectObjects(cubes.objects);
-
-        if (intersects.length > 0) {
-            
-            // disable controls
-            cubes.controls.enabled = false;
-
-            // set selection - first intersected object (closest)
-            cubes.selection = intersects[0].object;
-            console.log(intersects[0]);
-
-            // calculate offset
-            var intersects = cubes.raycaster.intersectObject(cubes.plane);
-            console.log(intersects[0]);
-            cubes.offset.copy(intersects[0].point).sub(cubes.plane.position);
-            
-        } else {
-            
-            
-            
-        }
-    },
-    
-    onDocumentMouseMove: function (event) {
-        
-        event.preventDefault();
-
-        // gget mouse position
-        var mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-        var mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
-
-        // get 3D vector from 3D mouse position using 'unproject' function
-        var vector = new THREE.Vector3(mouseX, mouseY, 1);
-        vector.unproject(cubes.camera);
-
-        // set raycaster position
         cubes.raycaster.set( cubes.camera.position, vector.sub( cubes.camera.position ).normalize() );
+        
+        switch (event.button) {
+            case 0:
+                if (cubes.selection) {
 
-        if (cubes.selection) {
-            
-            // check the position where the plane is intersected
-            var intersects = cubes.raycaster.intersectObject(cubes.plane);
-            // reposition the object based on the intersection point with the plane
-            cubes.selection.position.copy(intersects[0].point.sub(cubes.offset));
-            
-        } else {
-            
-            // update position of the plane if need
-            var intersects = cubes.raycaster.intersectObjects(cubes.objects);
-            
-            if (intersects.length > 0) {
+                    // check the position where the plane is intersected
+                    var intersects = cubes.raycaster.intersectObject(cubes.plane);
+                    // reposition the object based on the intersection point with the plane
+                    cubes.selection.position.copy(intersects[0].point.sub(cubes.offset));
+
+                } else {
+
+                    // update position of the plane if need
+                    var intersects = cubes.raycaster.intersectObjects(cubes.objects);
+
+                    if (intersects.length > 0) {
+
+                        cubes.plane.position.copy(intersects[0].object.position);
+                        cubes.plane.lookAt(cubes.camera.position);
+
+                    }
+                }
+                break;
                 
-                cubes.plane.position.copy(intersects[0].object.position);
-                cubes.plane.lookAt(cubes.camera.position);
+            case 1:
+                break;
                 
-            }
+            case 2:
+                var intersects = cubes.raycaster.intersectObject(cubes.plane);
+                var pos = intersects[0].point;
+                
+                var deltaMove = {
+                    x: pos.x - cubes.prevMousePosition.x,
+                    y: pos.y - cubes.prevMousePosition.y,
+                    z: pos.z - cubes.prevMousePosition.z
+                };
+                
+//                console.log(deltaMove);
+                
+                var deltaRotationquaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(cubes.toRadians(deltaMove.y * 1), cubes.toRadians(deltaMove.x * 1), 0, 'XYZ'));
+                cubes.selection.quaternion.multiplyQuaternions(deltaRotationquaternion, cubes.selection.quaternion);
+                
+                cubes.prevMousePosition = {
+                    x: pos.x,
+                    y: pos.y,
+                    z: pos.z
+                };
+                
+                break;
         }
     },
 
